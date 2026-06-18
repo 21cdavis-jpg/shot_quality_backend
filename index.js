498e5a771063372233d620c6b6d016d2b32c4b7d
Cal <21cdavis@gmail.com>
2026-06-17 23:08:17 -0400

    chore: adapt backend port binding for cloud deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
@@ -712,4 +712,6 @@ app.get('/api/league/summary', (req, res) => {
     });
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
