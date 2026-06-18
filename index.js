c14bda67d2d446fc84647ae7056f277e5454ea9f
Cal <21cdavis@gmail.com>
2026-06-18 00:09:31 -0400

    Fix static paths to dist and fix routing wildcards

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const db = new sqlite3.Database('./sports.db', (err) => {
     if (err) console.error(err.message);
@@ -710,6 +713,10 @@ app.get('/api/league/summary', (req, res) => {
             res.json(leagueSummary);
         });
     });
+});
+
+app.get('*catchall', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
